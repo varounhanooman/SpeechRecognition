53f3ef611df5767cdc565e1f698ccfc989c7f4a9
varounhanooman <varoun.hanooman@gmail.com>
2018-10-27 04:42:50 +0100

    updated pdf function

--- GMM.m
+++ GMM.m
@@ -9,8 +9,8 @@ clear
 %Initialize Values
 
 %y = [-0.39 0.12 0.94 1.67 1.76 2.44 3.72 4.28 4.92 5.53 0.06 0.48 1.01 1.68 1.80 3.25 4.12 4.60 5.28 6.22];
-y_1 = normrnd(22,2,[1,50]);
-y_2 = normrnd(12,2,[1,50]);
+y_1 = normrnd(22,2,[1,20]);
+y_2 = normrnd(12,2,[1,20]);
 y = [y_1,y_2];
 x = -1:30;
 Mu = [5 15];
@@ -19,12 +19,14 @@ pi = 0.1;
 wp1 = zeros(1, length(y)) ;
 wp2 = zeros(1, length(y)) ;
 
-for iteration=1:100
+for iteration=1:5000
     %Expectation Step
     %for each of the point, wich Gaussian generated it
 
-    wp1 = gaussmf(y,[sigma(1) Mu(1)])*pi;
-    wp2 = gaussmf(y,[sigma(2) Mu(2)])*(1-pi);
+    %wp1 = gaussmf(y,[sigma(1) Mu(1)])*pi;
+    %wp2 = gaussmf(y,[sigma(2) Mu(2)])*(1-pi);
+    wp1 = pdf('Normal',y,Mu(1),sigma(1))*pi;
+    wp2 = pdf('Normal',y,Mu(2),sigma(2))*(1-pi);
     dem = wp1+wp2;
     wp1 = wp1./dem;
     wp2 = wp2./dem;
@@ -45,15 +47,14 @@ for iteration=1:100
 
 end
 %no -> back to expecation step
- figure(1)
- hist(y,0:1:30);
- hold on
- plot(x, gaussmf(x,[5 5]), '--r', 'LineWidth',2)
- plot(x, gaussmf(x,[5 15]), '--g', 'LineWidth',2) 
- plot(x, gaussmf(x,[sigma(1) Mu(1)]), '-r', 'LineWidth',2)
- plot(x, gaussmf(x,[sigma(2) Mu(2)]), '-g', 'LineWidth',2) 
- hold off
-
-
+s =10;
+figure(1)
+hist(y,0:1:30);
+hold on
+plot(x, s*pdf('Normal',x,5,5), '--r', 'LineWidth',2)
+plot(x, s*pdf('Normal',x,15,5), '--g', 'LineWidth',2) 
+plot(x, s*pdf('Normal',x,Mu(1),sigma(1)), '-r', 'LineWidth',2)
+plot(x, s*pdf('Normal',x,Mu(2),sigma(2)), '-g', 'LineWidth',2) 
+hold off
 %yes -> Stop
 
